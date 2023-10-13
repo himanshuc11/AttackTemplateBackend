f019ab83d92125c3178996af574e1c70d1393891
himanshuc11 <himanshuchhatpar@gmail.com>
2023-10-13 17:45:53 +0530

    feat: created barebones tasks table

--- schema.ts
+++ schema.ts
@@ -1,4 +1,13 @@
-import { serial, varchar, text, pgTable, jsonb } from "drizzle-orm/pg-core";
+import {
+  serial,
+  varchar,
+  text,
+  pgTable,
+  jsonb,
+  boolean,
+  date,
+  time,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   emailId: text("email_id").primaryKey(),
@@ -10,4 +19,11 @@ export const tags = pgTable("tags", {
   id: serial("id"),
   color: varchar("color", { length: 7 }),
   userId: text("user_id").references(() => users.emailId),
+});
+
+export const tasks = pgTable("tasks", {
+  id: serial("id"),
+  isComplete: boolean("is_complete"),
+  dueDate: date("due_date"),
+  dueTime: time("due_time", { withTimezone: true }),
 });
