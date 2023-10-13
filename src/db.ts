39259dc96a50c25c9bb637680168a2ec3a611822
himanshuc11 <himanshuchhatpar@gmail.com>
2023-10-13 11:03:43 +0530

    refactor: Changed dependency from turso to neondb

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,11 @@
-import dotenv from "dotenv" 
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import dotenv from "dotenv";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV}`});
-const client = createClient({ url: process.env.DB_URL || "", authToken: process.env.DB_TOKEN || "" });
-const db = drizzle(client);
+neonConfig.fetchConnectionCache = true;
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const sql = neon(process.env.DB_URL!);
+const db = drizzle(sql);
 
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import dotenv from "dotenv" 
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-
-dotenv.config({ path: `.env.${process.env.NODE_ENV}`});
-const client = createClient({ url: process.env.DB_URL || "", authToken: process.env.DB_TOKEN || "" });
-const db = drizzle(client);
-
-export default db;
+import db from "./db.js";
